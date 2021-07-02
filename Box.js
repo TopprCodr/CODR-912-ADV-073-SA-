e5a77b64b5c90d8b8d5a93f042a3987d9168a034
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-07-02 17:37:15 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,7 +1,23 @@
-class Box extends BaseClass {
-  constructor(x, y, width, height){
-    super(x,y,width,height);
-    this.image = loadImage("sprites/wood1.png");
-  }
-
-};
+class Box {
+ constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        Composite.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos =this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+      }
+}
