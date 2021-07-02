6c0846e958febc300cccf6306d9eedd6092a27db
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-07-02 13:21:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,41 +3,26 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var ground, ball1,ball2,platform;
+var ground,platform;
 
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
-    var static_options ={
-        isStatic: true
-    }
+ 
 
-    ground = Bodies.rectangle(200,390,400,20,static_options);
-    World.add(world,ground);
-    platform = Bodies.rectangle(120,320,170,120,static_options);
-    World.add(world,platform);
+    ground = new Ground(200,390,400,20);
+    platform = new Ground(120,320,170,120);
     //create ball objects
-    ball1= new Ball(200,100,20)
-    ball2 = new Ball(270,50,10)
-    ball3= new Ball(120,100,20)
-    ball4 = new Ball(100,50,10)
+   
   
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
-    // to display the objects
-    rectMode(CENTER)
-    fill("brown");
-    rect(ground.position.x,ground.position.y,400,20);
-    fill("red");
-    rect(platform.position.x,platform.position.y,170,120);
+    // to display the object
   
-   ball1.display();
-   ball2.display()
-   ball3.display();
-   ball4.display();
-   
+    ground.display();
+    platform.display();
 }
