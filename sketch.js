dc80bdf63dace45b21316ab55afd888687684d12
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-07-02 17:41:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Composite = Matter.Composite;
 
 var engine, world;
-var ground,platform;
+var ground,platform,box1;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -13,8 +14,9 @@ function setup(){
 
     ground = new Ground(200,390,400,20);
     platform = new Ground(120,320,170,120);
-    //create ball objects
+    box1 = new Box(200,20,50,50);
    
+    // add ball objects
   
 }
 
@@ -22,7 +24,7 @@ function draw(){
     background(0);
     Engine.update(engine);
     // to display the object
-  
+    box1.display();
     ground.display();
     platform.display();
 }
